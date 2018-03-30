f24181511f40ecd6be9d1e3675339f12292555ec
camander321 <chamburg321@gmail.com>
2018-03-30 12:46:32 -0700

    undo broken styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { SuggestedVideoComponent } from './suggested-video/suggested-video.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderBarComponent,
-    SuggestedVideoComponent
+    SuggestedVideoComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule
